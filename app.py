c66c0cbd750fce60fd34f3b7bbd5b75a420c629a
Currycake Man <aidan.r.copeland@gmail.com>
2026-08-23 21:19:38 +1200

    V10

--- app.py
+++ app.py
@@ -4,3 +4,20 @@ from time import sleep
 
 onboard_led = Pin("LED", Pin.OUT)
 
+def morse_code(timeings):
+    for i in timeings:
+        onboard_led.on()
+        sleep(i)
+        onboard_led.off()
+        sleep(i)
+
+timeing = [1,1,1,1]
+
+morse_code(timeing)
+
+
+while True:
+    onboard_led.on()
+    sleep(1)
+    onboard_led.off()
+    sleep(1)
