43d44dfce58b37ef1427d2caac6782dda992fd9f
Currycake Man <aidan.r.copeland@gmail.com>
2026-08-23 16:00:15 +1200

    Fix Pico WH app hardware setup

--- app.py
+++ app.py
@@ -2,148 +2,91 @@ from machine import Pin, PWM
 from time import sleep
 
 
-
-on_bored_test = True
+# Enable only the hardware test you want to run.
+onboard_test = True
 rgb_test = False
 button_test = False
 rgb_button_test = False
 
 
-if on_bored_test == True:
-    On_bored_led = Pin("LED", Pin.OUT)
+if onboard_test:
+    onboard_led = Pin("LED", Pin.OUT)
 
-    for i in range(5):
-        On_bored_led.on()
+    for _ in range(5):
+        onboard_led.on()
         sleep(1)
-        On_bored_led.off()
+        onboard_led.off()
         sleep(1)
 
-    print("Passed onboard check")
+    print("Passed onboard LED check")
 
 
-if rgb_test == True:
-    rgb = [Pin(i, Pin.OUT) for i in range(16, 19)]
+if rgb_test:
+    rgb_pins = [Pin(pin, Pin.OUT) for pin in range(16, 19)]
 
-    for i in range(5):
-        for led in rgb:
+    for _ in range(5):
+        for led in rgb_pins:
             led.on()
             sleep(1)
             led.off()
 
-    print("Passed RGB check")
+    print("Passed RGB output check")
 
 
-if button_test == True:
+if button_test:
     red_led = Pin(0, Pin.OUT)
     button = Pin(15, Pin.IN, Pin.PULL_UP)
 
-    while button_test == True:
-        if button.value() == 0:
-            red_led.on()
-        else:
-            red_led.off()
-
+    while True:
+        red_led.value(button.value() == 0)
         sleep(0.01)
 
 
-
-
-def color(rgb):
-
-    # RGB PWM outputs
+if rgb_button_test:
+    # RGB PWM outputs. This assumes a common-cathode RGB LED.
     blue = PWM(Pin(16))
     green = PWM(Pin(17))
     red = PWM(Pin(18))
 
-    blue.freq(1000)
-    green.freq(1000)
-    red.freq(1000)
+    for output in (red, green, blue):
+        output.freq(1000)
+        output.duty_u16(0)
 
-    r = rgb[0]
-    g = rgb[1]
-    b = rgb[2]
+    def set_color(rgb):
+        red.duty_u16(rgb[0] * 257)
+        green.duty_u16(rgb[1] * 257)
+        blue.duty_u16(rgb[2] * 257)
 
-    red.duty_u16(r * 257)
-    green.duty_u16(g * 257)
-    blue.duty_u16(b * 257)
-
-
-if rgb_button_test == True:
-
-    # Indicator LED/output
-    sing = Pin(19, Pin.OUT)
-
-    # Toggle button
-    sing_button = Pin(7, Pin.IN, Pin.PULL_UP)
-
-    # RGB buttons
-    r, g, b = [
-        Pin(i, Pin.IN, Pin.PULL_UP)
-        for i in range(12, 15)
+    indicator = Pin(19, Pin.OUT)
+    direction_button = Pin(7, Pin.IN, Pin.PULL_UP)
+    red_button, green_button, blue_button = [
+        Pin(pin, Pin.IN, Pin.PULL_UP) for pin in range(12, 15)
     ]
 
-    num = 30
-    rgb_list = [0, 0, 0]
-
-    # False = increase
-    # True = decrease
+    amount = 30
+    rgb = [0, 0, 0]
     decrease_mode = False
-
-    last_toggle = 1
+    last_direction = 1
 
     while True:
+        current_direction = direction_button.value()
 
-        # Detect one press of the toggle button
-        current_toggle = sing_button.value()
-
-        if current_toggle == 0 and last_toggle == 1:
+        if current_direction == 0 and last_direction == 1:
             decrease_mode = not decrease_mode
+            indicator.value(decrease_mode)
+            print("Mode:", "DOWN" if decrease_mode else "UP")
+            sleep(0.05)
 
-            if decrease_mode:
-                print("Mode: DOWN")
-                sing.on()
-            else:
-                print("Mode: UP")
-                sing.off()
+        last_direction = current_direction
+        change = -amount if decrease_mode else amount
 
-            sleep(0.05)  # debounce
-
-        last_toggle = current_toggle
-
-        # Decide whether RGB buttons add or subtract
-        if decrease_mode:
-            inter = -num
-        else:
-            inter = num
-
-        # RED button
-        if r.value() == 0:
-            rgb_list[0] = max(
-                0,
-                min(rgb_list[0] + inter, 255)
-            )
-            color(rgb_list)
-            print(rgb_list)
-            sleep(0.15)
-
-        # GREEN button
-        if g.value() == 0:
-            rgb_list[1] = max(
-                0,
-                min(rgb_list[1] + inter, 255)
-            )
-            color(rgb_list)
-            print(rgb_list)
-            sleep(0.15)
-
-        # BLUE button
-        if b.value() == 0:
-            rgb_list[2] = max(
-                0,
-                min(rgb_list[2] + inter, 255)
-            )
-            color(rgb_list)
-            print(rgb_list)
-            sleep(0.15)
+        for index, button in enumerate(
+            (red_button, green_button, blue_button)
+        ):
+            if button.value() == 0:
+                rgb[index] = max(0, min(rgb[index] + change, 255))
+                set_color(rgb)
+                print(rgb)
+                sleep(0.15)
 
         sleep(0.01)
