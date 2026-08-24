afe33156b9f10f3cacd1c3aa867f90baad00202c
Currycake Man <aidan.r.copeland@gmail.com>
2026-08-24 16:44:21 +1200

    V15

--- app.py
+++ app.py
@@ -1,13 +1,41 @@
 from machine import Pin
 from time import sleep
 
-red_led = Pin(1, Pin.OUT)
+on_off_led = Pin("LED",Pin.OUT)
+on_off_led.on()
+
+
+
 button = Pin(15, Pin.IN, Pin.PULL_UP)
+leds = [Pin(pin, Pin.OUT) for pin in (0, 1,)]
+
+
+
+num = 0
+
+def update_led(num):
+
+    biner = [int(x) for x in f"{num:02b}"]
+
+    led = -1
+    for item in biner:
+        led += 1
+        if item == 1:
+            leds[led].on()
+        if item == 0:
+            leds[led].off()
+
+
+update_led(num)
 
 while True:
-    if button.value() == 0:
-        red_led.on()
-    else:
-        red_led.off()
+    while button.value() == 1:
+        sleep(0.1)
+    num += 1
 
-    sleep(0.01)
+    if num > 3:
+        num = 0
+
+    update_led(num)
+    while button.value() == 0:
+        sleep(0.01)
