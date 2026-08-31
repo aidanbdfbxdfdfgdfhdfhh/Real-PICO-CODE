4dbc7f27c7dd75ce46c9e0109a23cbf1837cf745
Currycake Man <aidan.r.copeland@gmail.com>
2026-08-31 17:29:12 +1200

    VGA test 2 V30

--- app.py
+++ app.py
@@ -1,58 +1,25 @@
-from machine import Pin
-import time
+from machine import Pin, PWM
+from time import sleep
 
-row1 = Pin(0, Pin.OUT)
-row2 = Pin(1, Pin.OUT)
-row3 = Pin(2, Pin.OUT)
+# VGA 640x480-ish sync test
 
-col1 = Pin(3, Pin.OUT)
-col2 = Pin(4, Pin.OUT)
-col3 = Pin(5, Pin.OUT)
+# Horizontal sync
+hsync = PWM(
+    Pin(14),
+    freq=31469,
+    duty_ns=3813,
+    invert=True
+)
 
-rows = [row1, row2, row3]
-cols = [col1, col2, col3]
+# Vertical sync
+vsync = PWM(
+    Pin(16),
+    freq=60,
+    duty_ns=63500,
+    invert=True
+)
 
+print("VGA sync running")
 
-def turn_on_leds(*leds):
-        for led in leds:
-
-            if led < 1 or led > 9:
-                continue
-
-            # Everything OFF first
-            for row in rows:
-                row.value(0)
-
-            for col in cols:
-                col.value(1)
-
-            index = led - 1
-            row = index // 3
-            col = index % 3
-
-            # Selected LED ON
-            rows[row].value(1)
-            cols[col].value(0)
-
-            time.sleep_ms(2)
-
-
-state1 = 1
-state2 = 5
-state3 = 9
 while True:
-    start = time.ticks_ms()
-
-    while time.ticks_diff(time.ticks_ms(), start) < 1000:
-        turn_on_leds(state1, state2, state3)
-        
-    state1 +=  1
-    state2 += 1
-    state3 += 1
-
-    if state1 == 4:
-        state1 = 1
-    if state2 == 7:
-        state2 = 4
-    if state3 == 10:
-        state3 = 7
+    sleep(1)
