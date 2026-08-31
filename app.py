c4435b302e682f4374ffb3196a656ef281aef372
Currycake Man <aidan.r.copeland@gmail.com>
2026-08-31 12:53:47 +1200

    V28

--- app.py
+++ app.py
@@ -1,25 +1,30 @@
 from machine import Pin
 import time
 
-on_bored_led = Pin("led",Pin.OUT)
+# Pico onboard LED
+on_board_led = Pin("LED", Pin.OUT)
 
+
+
+# Rows
 row1 = Pin(0, Pin.OUT)
+row2 = Pin(1, Pin.OUT)
+row3 = Pin(2, Pin.OUT)
 
+# Columns
 col1 = Pin(3, Pin.OUT)
 col2 = Pin(4, Pin.OUT)
 col3 = Pin(5, Pin.OUT)
 
-# Turn row 1 on
+# Only Row 1 active
 row1.value(1)
+row2.value(0)
+row3.value(0)
 
-# First LED ON
+# All 3 LEDs in Row 1 ON
 col1.value(0)
-
-# Other two OFF
-col2.value(1)
-col3.value(1)
+col2.value(0)
+col3.value(0)
 
 while True:
-    time.sleep(4)
-    col2.value(0)
-    col3.value(0)
+    time.sleep(1)
